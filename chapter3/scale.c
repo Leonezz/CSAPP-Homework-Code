afa9d022ae9bebb27240b2a6fd3fa8de37149952
zhuwenq <lonelywolf12138@outlook.com>
2021-07-25 21:29:48 +0800

    update chapter3/scale.c

--- chapter3/scale.c
+++ chapter3/scale.c
@@ -4,3 +4,11 @@ int64_t scale(int64_t x, int64_t y, int64_t z)
     int64_t t = x + 4 * y + 12 * z;
     return t;
 }
+
+/*
+scale:
+	leaq	(%rdi,%rsi,4), %rax
+	leaq	(%rdx,%rdx,2), %rdx
+	leaq	(%rax,%rdx,4), %rax
+	ret
+*/
